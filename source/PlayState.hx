f15469a61a08cd442eb2ea718d5feda42b6096af
RedstoneRuler <earthstar67876@gmail.com>
2022-03-26 08:47:42 -0400

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1966,11 +1966,11 @@ class PlayState extends MusicBeatState
 		else {
 		*/
 			hit = Math.abs(strumtime - Conductor.songPosition / 10);
-			FlxG.watch.addQuick('Hit', hit);
+			FlxG.watch.addQuick('Hit', Math.abs(strumtime - Conductor.songPosition) / 10);
 			FlxG.watch.addQuick('Accuracy Increment', (strumtime - Conductor.songPosition) / 10 / (accuracy / 20) / (totalNotes / 25));
 
 			//some leniency to make 100% accuracy actually possible
-			if(hit > 1 || hit < 6.5) {
+			if(Math.abs(strumtime - Conductor.songPosition / 10) > 1 || Math.abs(strumtime - Conductor.songPosition / 10) < 6.5) {
 				accuracy += Math.abs(strumtime - Conductor.songPosition) / 10 / (accuracy / 20) / (totalNotes / 25);
 			} else {
 				accuracy += (strumtime - Conductor.songPosition) / 10 / (accuracy / 20) / (totalNotes / 25);

0c51f6ece22b6878dc2347f1901f0255617db6e5
RedstoneRuler <earthstar67876@gmail.com>
2022-04-18 12:51:54 -0400

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -519,7 +519,7 @@ class PlayState extends MusicBeatState
 			waveSprite.scale.set(6, 6);
 			waveSpriteFG.scale.set(6, 6);
 			waveSprite.setPosition(posX + 80, posY + 165);
-			waveSpriteFG.setPosition(posX + 90, posY + 165);
+			waveSpriteFG.setPosition(posX + 80, posY + 165);
 
 			waveSprite.scrollFactor.set(0.7, 0.8);
 			waveSpriteFG.scrollFactor.set(0.9, 0.8);
